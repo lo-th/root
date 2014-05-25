1e03e5af887aced5afc9c133fef61eabed8b37cd
lo-th <3dflashlo@gmail.com>
2014-05-25 22:10:08 +0200

    test

---
+++ js/worker.oimo.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var world;
+var timer;
+var infos = [];
+
+self.onmessage = function (e) {
+	var p = e.data.tell;
+
+	if(p == "INIT"){
+		importScripts(e.data.url);
+		// Init world
+        OIMO.WORLD_SCALE = 1;
+        OIMO.INV_SCALE = 1;
+        world = new OIMO.World(1/60, 2, 8);
+        world.gravity.init(0,-10,0);
+        timer = setInterval(update, 1000/60); 
+	}
+}
+
+var update = function(){
+	world.step();
+	infos[0] = world.performance.fpsint;
+	self.postMessage({tell:"RUN", infos:infos});
+}
