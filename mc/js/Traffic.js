4056abe4ebe523076f285a7858a18f9208c2163f
lo-th <3dflashlo@gmail.com>
2015-05-09 18:21:22 +0200

    up

--- mc/js/Traffic.js
+++ mc/js/Traffic.js
@@ -505,7 +505,7 @@ Traffic.NetWork.prototype = {
 
     		if(cubic==2){
     			c = new THREE.Line( this.carBox[car.type], this.box_car_mat, THREE.LinePieces );
-    			c.frustumCulled = false;
+    			//c.frustumCulled = false;
     			//c = new THREE.Mesh( this.getGeometry('cars', TRAFFIC.TYPE_OF_CARS[car.type].m), this.car_mat[r] );
     			//c = new THREE.Mesh( this.carLow[car.type], this.car_mat[r] );
     			//var b = new THREE.BoxHelper(c);
@@ -680,53 +680,35 @@ V.SimpleBox.prototype = Object.create( THREE.BufferGeometry.prototype );
 V.SimpleBox.prototype.constructor = V.SimpleBox;
 
 V.SimpleBox.prototype.makeBox = function ( min, max ) {
-
 	var vertices = this.attributes.position.array;
-
 	vertices[  0 ] = max.x; vertices[  1 ] = max.y; vertices[  2 ] = max.z;
 	vertices[  3 ] = min.x; vertices[  4 ] = max.y; vertices[  5 ] = max.z;
-
 	vertices[  6 ] = min.x; vertices[  7 ] = max.y; vertices[  8 ] = max.z;
 	vertices[  9 ] = min.x; vertices[ 10 ] = min.y; vertices[ 11 ] = max.z;
-
 	vertices[ 12 ] = min.x; vertices[ 13 ] = min.y; vertices[ 14 ] = max.z;
 	vertices[ 15 ] = max.x; vertices[ 16 ] = min.y; vertices[ 17 ] = max.z;
-
 	vertices[ 18 ] = max.x; vertices[ 19 ] = min.y; vertices[ 20 ] = max.z;
 	vertices[ 21 ] = max.x; vertices[ 22 ] = max.y; vertices[ 23 ] = max.z;
-
-	//
-
 	vertices[ 24 ] = max.x; vertices[ 25 ] = max.y; vertices[ 26 ] = min.z;
 	vertices[ 27 ] = min.x; vertices[ 28 ] = max.y; vertices[ 29 ] = min.z;
-
 	vertices[ 30 ] = min.x; vertices[ 31 ] = max.y; vertices[ 32 ] = min.z;
 	vertices[ 33 ] = min.x; vertices[ 34 ] = min.y; vertices[ 35 ] = min.z;
-
 	vertices[ 36 ] = min.x; vertices[ 37 ] = min.y; vertices[ 38 ] = min.z;
 	vertices[ 39 ] = max.x; vertices[ 40 ] = min.y; vertices[ 41 ] = min.z;
-
 	vertices[ 42 ] = max.x; vertices[ 43 ] = min.y; vertices[ 44 ] = min.z;
 	vertices[ 45 ] = max.x; vertices[ 46 ] = max.y; vertices[ 47 ] = min.z;
-
-	//
-
 	vertices[ 48 ] = max.x; vertices[ 49 ] = max.y; vertices[ 50 ] = max.z;
 	vertices[ 51 ] = max.x; vertices[ 52 ] = max.y; vertices[ 53 ] = min.z;
-
 	vertices[ 54 ] = min.x; vertices[ 55 ] = max.y; vertices[ 56 ] = max.z;
 	vertices[ 57 ] = min.x; vertices[ 58 ] = max.y; vertices[ 59 ] = min.z;
-
 	vertices[ 60 ] = min.x; vertices[ 61 ] = min.y; vertices[ 62 ] = max.z;
 	vertices[ 63 ] = min.x; vertices[ 64 ] = min.y; vertices[ 65 ] = min.z;
-
 	vertices[ 66 ] = max.x; vertices[ 67 ] = min.y; vertices[ 68 ] = max.z;
 	vertices[ 69 ] = max.x; vertices[ 70 ] = min.y; vertices[ 71 ] = min.z;
-
 	this.attributes.position.needsUpdate = true;
 	this.computeBoundingSphere();
 
 	//this.matrix = object.matrixWorld;
-	this.matrixAutoUpdate = true;
+	//this.matrixAutoUpdate = true;
 
 };
