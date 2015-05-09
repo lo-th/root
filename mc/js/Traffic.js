566560422bcc64f632779995a6d8ad832400762f
lo-th <3dflashlo@gmail.com>
2015-05-09 18:15:38 +0200

    op

--- mc/js/Traffic.js
+++ mc/js/Traffic.js
@@ -59,6 +59,12 @@ Traffic.NetWork.prototype = {
         var g = this.meshes[obj][name].geometry;
         return g;
     },
+    getBoxGeometry:function(obj, ref){
+    	var box = this.meshes[obj][ref.m].geometry.boundingBox;
+    	//var g = new V.SimpleBox({x:-ref.w, y:-ref.h, z:-ref.l},{x:-ref.w, y:-ref.h, z:-ref.l});
+    	var g = new V.SimpleBox( box.min, box.max );
+    	return g;
+    },
     getHighGeometry:function(obj, ref){
     	var name = ref.m;
     	var num = name.substring(3,name.length);
@@ -311,7 +317,7 @@ Traffic.NetWork.prototype = {
 		this.generateCarGeometry();
 	},
 
-	addBackGround:function(){
+	/*addBackGround:function(){
 		var t = (((7*4)+1)*this.grid);
 		var g = new THREE.PlaneBufferGeometry( t, t*0.5 );
 		var pos = [
@@ -332,22 +338,10 @@ Traffic.NetWork.prototype = {
             //debugMode:true,
         };
 
-		//this.groundMirror = new THREE.Mirror( this.root.renderer,this.root.cameras[this.root.currentCamera], settings );
-
 		this.groundMirror1 = new THREE.Mirror( this.root.renderer,this.root.nav.camera, settings );
 		this.groundMirror2 = new THREE.Mirror( this.root.renderer,this.root.nav.camera, settings );
 		this.groundMirror3 = new THREE.Mirror( this.root.renderer,this.root.nav.camera, settings );
 		this.groundMirror4 = new THREE.Mirror( this.root.renderer,this.root.nav.camera, settings );
-
-		/*this.groundMirror1 = new THREE.Mirror( this.root.renderer,null, settings, pos[0] );
-		this.groundMirror2 = new THREE.Mirror( this.root.renderer,null, settings, pos[1] );
-		this.groundMirror3 = new THREE.Mirror( this.root.renderer,null, settings, pos[2] );
-		this.groundMirror4 = new THREE.Mirror( this.root.renderer,null, settings, pos[3] );*/
-
-		//var myMirror = new THREE.FlatMirror(this.root.renderer,this.root.nav.camera, {clipBias: 0.005, textureWidth: this.root.dimentions.w, textureHeight: this.root.dimentions.h } );
-		
-
-		//this.back_mat = this.groundMirror.material;
 		
 		var bg1 = new THREE.Mesh( g, this.groundMirror1.material );
 		var bg2 = new THREE.Mesh( g, this.groundMirror2.material );
@@ -381,11 +375,7 @@ Traffic.NetWork.prototype = {
 		bg2.position.set( 0, (t*0.25)+this.decal.y, t*0.5);
 		bg3.position.set( -t*0.5, (t*0.25)+this.decal.y, 0);
 		bg4.position.set( t*0.5, (t*0.25)+this.decal.y, 0);
-		/*bg1.position.set( 0,this.decal.y, -t*0.5);
-		bg2.position.set( 0, this.decal.y, t*0.5);
-		bg3.position.set( -t*0.5, this.decal.y, 0);
-		bg4.position.set( t*0.5, this.decal.y, 0);*/
-	},
+	},*/
 
 	// GRID
 
@@ -491,13 +481,15 @@ Traffic.NetWork.prototype = {
 		}
 	},
 	generateCarGeometry : function(){
-		this.carLow = [];
+		//this.carLow = [];
 		this.carHigh = [];
+		this.carBox = [];
 
 		var i = 14;
 		while(i--){
-			this.carLow[i] = this.getGeometry('cars', TRAFFIC.TYPE_OF_CARS[i].m);
+			//this.carLow[i] = this.getGeometry('cars', TRAFFIC.TYPE_OF_CARS[i].m);
 		    this.carHigh[i] = this.getHighGeometry('cars', TRAFFIC.TYPE_OF_CARS[i]);
+		    this.carBox[i] = this.getBoxGeometry('cars', TRAFFIC.TYPE_OF_CARS[i]);
 		}
 
 	},
@@ -512,18 +504,20 @@ Traffic.NetWork.prototype = {
     		var c;
 
     		if(cubic==2){
+    			c = new THREE.Line( this.carBox[car.type], this.box_car_mat, THREE.LinePieces );
+    			c.frustumCulled = false;
     			//c = new THREE.Mesh( this.getGeometry('cars', TRAFFIC.TYPE_OF_CARS[car.type].m), this.car_mat[r] );
-    			c = new THREE.Mesh( this.carLow[car.type], this.car_mat[r] );
-    			var b = new THREE.BoxHelper(c);
-    			b.material = this.box_car_mat;
-    			this.content.add( b );
-    			c.visible = false;
+    			//c = new THREE.Mesh( this.carLow[car.type], this.car_mat[r] );
+    			//var b = new THREE.BoxHelper(c);
+    			//b.material = this.box_car_mat;
+    			//this.content.add( b );
+    			//c.visible = false;
     		}else{
-    			//c = new THREE.Mesh( this.getHighGeometry('cars', TRAFFIC.TYPE_OF_CARS[car.type]), this.car_mat[r] );
     			c = new THREE.Mesh( this.carHigh[car.type], this.car_mat[r] );
     		}
-    		    this.content.add( c );
-    		    this.cars[id] = c;
+    		   
+    		this.content.add( c );
+    		this.cars[id] = c;
     		//}
     		
     		//c.scale.set(car.length, car.length/2, car.width);
@@ -669,3 +663,70 @@ Traffic.NetWork.prototype = {
 		return resl;
 	}
 }
+
+
+V.SimpleBox = function ( min, max ) {
+	THREE.BufferGeometry.call( this );
+	//var geometry = new THREE.BufferGeometry();
+	this.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( 72 ), 3 ) );
+
+	//THREE.Line.call( this, geometry, mat, THREE.LinePieces );
+
+	this.makeBox( min, max );
+}
+
+//V.SimpleBox.prototype = Object.create( THREE.Line.prototype );
+V.SimpleBox.prototype = Object.create( THREE.BufferGeometry.prototype );
+V.SimpleBox.prototype.constructor = V.SimpleBox;
+
+V.SimpleBox.prototype.makeBox = function ( min, max ) {
+
+	var vertices = this.attributes.position.array;
+
+	vertices[  0 ] = max.x; vertices[  1 ] = max.y; vertices[  2 ] = max.z;
+	vertices[  3 ] = min.x; vertices[  4 ] = max.y; vertices[  5 ] = max.z;
+
+	vertices[  6 ] = min.x; vertices[  7 ] = max.y; vertices[  8 ] = max.z;
+	vertices[  9 ] = min.x; vertices[ 10 ] = min.y; vertices[ 11 ] = max.z;
+
+	vertices[ 12 ] = min.x; vertices[ 13 ] = min.y; vertices[ 14 ] = max.z;
+	vertices[ 15 ] = max.x; vertices[ 16 ] = min.y; vertices[ 17 ] = max.z;
+
+	vertices[ 18 ] = max.x; vertices[ 19 ] = min.y; vertices[ 20 ] = max.z;
+	vertices[ 21 ] = max.x; vertices[ 22 ] = max.y; vertices[ 23 ] = max.z;
+
+	//
+
+	vertices[ 24 ] = max.x; vertices[ 25 ] = max.y; vertices[ 26 ] = min.z;
+	vertices[ 27 ] = min.x; vertices[ 28 ] = max.y; vertices[ 29 ] = min.z;
+
+	vertices[ 30 ] = min.x; vertices[ 31 ] = max.y; vertices[ 32 ] = min.z;
+	vertices[ 33 ] = min.x; vertices[ 34 ] = min.y; vertices[ 35 ] = min.z;
+
+	vertices[ 36 ] = min.x; vertices[ 37 ] = min.y; vertices[ 38 ] = min.z;
+	vertices[ 39 ] = max.x; vertices[ 40 ] = min.y; vertices[ 41 ] = min.z;
+
+	vertices[ 42 ] = max.x; vertices[ 43 ] = min.y; vertices[ 44 ] = min.z;
+	vertices[ 45 ] = max.x; vertices[ 46 ] = max.y; vertices[ 47 ] = min.z;
+
+	//
+
+	vertices[ 48 ] = max.x; vertices[ 49 ] = max.y; vertices[ 50 ] = max.z;
+	vertices[ 51 ] = max.x; vertices[ 52 ] = max.y; vertices[ 53 ] = min.z;
+
+	vertices[ 54 ] = min.x; vertices[ 55 ] = max.y; vertices[ 56 ] = max.z;
+	vertices[ 57 ] = min.x; vertices[ 58 ] = max.y; vertices[ 59 ] = min.z;
+
+	vertices[ 60 ] = min.x; vertices[ 61 ] = min.y; vertices[ 62 ] = max.z;
+	vertices[ 63 ] = min.x; vertices[ 64 ] = min.y; vertices[ 65 ] = min.z;
+
+	vertices[ 66 ] = max.x; vertices[ 67 ] = min.y; vertices[ 68 ] = max.z;
+	vertices[ 69 ] = max.x; vertices[ 70 ] = min.y; vertices[ 71 ] = min.z;
+
+	this.attributes.position.needsUpdate = true;
+	this.computeBoundingSphere();
+
+	//this.matrix = object.matrixWorld;
+	this.matrixAutoUpdate = true;
+
+};
