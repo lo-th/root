3997c527392ede1679882c9837af12f5a6d6e1ef
lo-th <3dflashlo@gmail.com>
2016-04-05 22:38:37 +0200

    up

--- normal/js/NormalMaker.js
+++ normal/js/NormalMaker.js
@@ -1,10 +1,20 @@
 THREE.NormalMaker = function( renderer ){
 
+    this.w = 0;
+    this.h = 0;
+
     this.sharpen = 0;
     this.blur = 0;
     this.emboss = 0;
 
-    this.pixels = null;
+    this.Dither = 0;
+
+    this.Rblur = 0;
+
+    this.quality = 1;
+    this.blurX = 1;
+    this.blurY = 1;
+
     this.finalPixels = null;
 
     this.renderer = renderer;
@@ -19,7 +29,8 @@ THREE.NormalMaker = function( renderer ){
     this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
     this.camera.position.z = 1;
     this.scene = new THREE.Scene();
-    this.plane = null;//new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), this.material );
+    this.plane = null;
+    //new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), this.material );
    // var sph2 = new THREE.Mesh( new THREE.SphereBufferGeometry(0.8, 30, 30), this.material );
     //this.scene.add( this.plane );
     //this.scene.add( sph2 );
@@ -47,12 +58,12 @@ THREE.NormalMaker.prototype = {
             var canvas = document.createElement('canvas');
             canvas.width = img.width;
             canvas.height = img.height;
+
+            this.w = canvas.width;
+            this.h = canvas.height;
             this.ctx = canvas.getContext("2d");
 
             this.ctx.drawImage(img,0,0);
-
-            this.pixels = this.ctx.getImageData(0, 0, canvas.width, canvas.height);
-
             
             this.makeTerrain();
 
@@ -62,63 +73,20 @@ THREE.NormalMaker.prototype = {
 
     },
 
-    convolute : function( weights, opaque) {
-        var side = Math.round(Math.sqrt(weights.length));
-        var halfSide = Math.floor(side/2);
-        var src = this.pixels.data;
-        var sw = this.pixels.width;
-        var sh = this.pixels.height;
-        // pad output by the convolution matrix
-        var w = sw;
-        var h = sh;
-        this.finalPixels = this.ctx.createImageData(w, h);
-        var dst = this.finalPixels.data;
-        // go through the destination image pixels
-        var alphaFac = opaque ? 1 : 0;
-        for (var y=0; y<h; y++) {
-            for (var x=0; x<w; x++) {
-                var sy = y;
-                var sx = x;
-                var dstOff = (y*w+x)*4;
-                // calculate the weighed sum of the source image pixels that
-                // fall under the convolution matrix
-                var r=0, g=0, b=0, a=0;
-                for (var cy=0; cy<side; cy++) {
-                   for (var cx=0; cx<side; cx++) {
-                        var scy = sy + cy - halfSide;
-                        var scx = sx + cx - halfSide;
-                        if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
-                            var srcOff = (scy*sw+scx)*4;
-                            var wt = weights[cy*side+cx];
-                            r += src[srcOff] * wt;
-                            g += src[srcOff+1] * wt;
-                            b += src[srcOff+2] * wt;
-                            a += src[srcOff+3] * wt;
-                        }
-                    }
-                }
-               dst[dstOff] = r;
-               dst[dstOff+1] = g;
-               dst[dstOff+2] = b;
-               dst[dstOff+3] = a + alphaFac*(255-a);
-            }
-        }
-
-        //this.finalPixels = output;
-        //return output;
-    },
-
     makeTerrain:function( ){
-        if( this.emboss !== 0 ) this.convolute( [  -2, -1,  0, -1,  1, 1, 0, 1,  2 ], false );
-        if( this.sharpen !== 0 ) this.convolute( [  0, -1,  0, -1,  5, -1, 0, -1,  0 ], false );
-        if( this.blur !== 0 ){ 
-            var b = 1 / ( this.blur * this.blur );
-            this.convolute( [ b, b, b, b, b, b, b, b, b ], false );
-        }
 
-        if(this.blur===0 && this.sharpen === 0) this.finalPixels = this.pixels;
+        // http://www.arahaya.com/imagefilters/
 
-        var g = new THREE.PlaneBufferGeometry( 2, 2, this.pixels.width-1, this.pixels.height-1 );
+        this.finalPixels = this.ctx.getImageData(0, 0, this.w, this.h);;
+
+        if( this.blur !== 0 ) this.finalPixels = ImageFilters.GaussianBlur( this.finalPixels, this.blur );
+        if( this.blurX > 1 || this.blurY > 1 ) this.finalPixels = ImageFilters.BoxBlur( this.finalPixels, this.blurX, this.blurY, this.quality);
+        if( this.Rblur !== 0 ) this.finalPixels = ImageFilters.StackBlur( this.finalPixels, this.Rblur );
+        if( this.Dither  !== 0 ) this.finalPixels = ImageFilters.Dither( this.finalPixels, this.Dither );
+        if( this.sharpen  !== 0 ) this.finalPixels = ImageFilters.Sharpen( this.finalPixels, this.sharpen );
+
+
+        var g = new THREE.PlaneBufferGeometry( 2, 2, this.w-1, this.h-1 );
         var vertices = g.attributes.position.array;
 
         var i = vertices.length/3;
