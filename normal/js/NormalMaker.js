bb6f508e64c7eac5ca8df4c4d6cc22f98ad634af
lo-th <3dflashlo@gmail.com>
2016-04-06 08:56:21 +0200

    up

--- normal/js/NormalMaker.js
+++ normal/js/NormalMaker.js
@@ -1,5 +1,7 @@
 THREE.NormalMaker = function( renderer ){
 
+    this.timer = null;
+
     this.w = 0;
     this.h = 0;
 
@@ -29,7 +31,10 @@ THREE.NormalMaker = function( renderer ){
     this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
     this.camera.position.z = 1;
     this.scene = new THREE.Scene();
+
     this.plane = null;
+    this.geometry = null;
+
     //new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), this.material );
    // var sph2 = new THREE.Mesh( new THREE.SphereBufferGeometry(0.8, 30, 30), this.material );
     //this.scene.add( this.plane );
@@ -51,6 +56,8 @@ THREE.NormalMaker.prototype = {
     },
 
     load : function( url ){
+
+        this.dispose();
 
         var img = new Image();
         img.onload = function(){
@@ -75,10 +82,17 @@ THREE.NormalMaker.prototype = {
 
     makeTerrain:function( ){
 
+        var newgeo = this.geometry === null ? true : false;
+
+        
+
+        this.finalPixels = this.ctx.getImageData( 0, 0, this.w, this.h );
+
+        //if( this.Rblur !== 0 ) this.finalPixels = stackBlurCanvasRGB( this.finalPixels, this.w, this.h, this.Rblur );
+
+
+
         // http://www.arahaya.com/imagefilters/
-
-        this.finalPixels = this.ctx.getImageData(0, 0, this.w, this.h);;
-
         if( this.blur !== 0 ) this.finalPixels = ImageFilters.GaussianBlur( this.finalPixels, this.blur );
         if( this.blurX > 1 || this.blurY > 1 ) this.finalPixels = ImageFilters.BoxBlur( this.finalPixels, this.blurX, this.blurY, this.quality);
         if( this.Rblur !== 0 ) this.finalPixels = ImageFilters.StackBlur( this.finalPixels, this.Rblur );
@@ -86,25 +100,32 @@ THREE.NormalMaker.prototype = {
         if( this.sharpen  !== 0 ) this.finalPixels = ImageFilters.Sharpen( this.finalPixels, this.sharpen );
 
 
-        var g = new THREE.PlaneBufferGeometry( 2, 2, this.w-1, this.h-1 );
-        var vertices = g.attributes.position.array;
+        if( newgeo ) this.geometry = new THREE.PlaneBufferGeometry( 2, 2, this.w-1, this.h-1 );
 
+        var vertices = this.geometry.attributes.position.array;
         var i = vertices.length/3;
         var n3, n4, v;
         var data = this.finalPixels.data;
 
         while(i--){
+        //for(var i=0; i< lng ; i++ ){
             n3 = i*3;
             n4 = i*4;
             // CIE luminance for the RGB
             v = 0.2126*data[n4] + 0.7152*data[n4+1] + 0.0722*data[n4+2];
-            vertices[n3+2] = v / 255;
+            vertices[n3+2] = v * 0.00392156862745098 /// 255;
         }
 
-        g.computeVertexNormals();
+        if( !newgeo ) this.geometry.attributes.position.needsUpdate = true;
+        this.geometry.computeVertexNormals();
 
-        this.plane = new THREE.Mesh( g, this.material );
-        this.scene.add( this.plane );
+        if( newgeo ){
+            this.plane = new THREE.Mesh( this.geometry, this.material );
+            this.scene.add( this.plane );
+        }
+
+        //clearTimeout( this.timer1 );
+        //this.timer1 = setTimeout( this.render.bind(this) , 17 );
 
         this.render();
 
@@ -112,10 +133,36 @@ THREE.NormalMaker.prototype = {
 
     render : function(){
 
-        this.renderer.render( this.scene, this.camera, this.renderTarget, true );
+        this.renderer.render( this.scene, this.camera, this.renderTarget, false );
+
+        clearTimeout( this.timer );
+        this.timer = setTimeout( this.dispose.bind(this) , 17 );
+
+    },
+
+    dispose : function(){
+
+        if(this.geometry === null) return;
 
         this.scene.remove( this.plane );
-        this.plane.geometry.dispose();
+        this.geometry.dispose();
+
+        this.plane = null;
+        this.geometry = null;
+
+    },
+
+    save:function(){
+        if(this.geometry === null) this.makeTerrain();
+
+
+        var renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( this.w, this.h );
+
+        renderer.render( this.scene, this.camera ); 
+
+        window.open( renderer.domElement.toDataURL( 'image/png' ), 'screenshot' );
 
     }
 
