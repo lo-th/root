3e5552e370809d2de786928e296807865ff19f28
lo-th <3dflashlo@gmail.com>
2017-01-09 12:57:21 +0100

    up

--- private1/js/Model.js
+++ private1/js/Model.js
@@ -1,77 +1,75 @@
 var V = {};
 
-V.Model = function ( Scene, type, meshs, mat, pos ) {
+V.Model = function ( Scene, type, meshs, txt, pos ) {
 
     var tSize = 1.4;
-    var debug = false;
+    this.debug = false;
 
-    
+    //this.root = new THREE.Group();
 
-
-    //this.mesh = meshs.body.clone();
     this.mesh = meshs.skin.clone();
+
+    this.mesh.name = type;
 
     this.bones = this.mesh.skeleton.bones;
 
     var i, name, bone, lng = this.bones.length;
 
+    this.poseMatrix = [];
     this.b = {};
 
     for( i=0; i<lng; i++){
+
         bone = this.bones[i];
         name = bone.name;
         this.b[ name ] = bone;
-        //if(type === 'man') console.log(i, this.bones[i].name )
+        this.poseMatrix[i] = bone.matrixWorld.clone();
+
     }
 
-    //console.log(this.b.head)
-    //console.log(this.b.rHand)
-
-    //var a0 = meshs.corps.clone();
-    //a0.rotation.set( 180*torad,180*torad, 0*torad );
-
     var a1 = meshs.head_mesh.clone();
-    a1.rotation.set( 180*torad,180*torad, 0*torad );
+    a1.rotation.set( 180*torad, 180*torad, 0 );
 
     var a2 = type === 'man' ? meshs.extra_man.clone() : meshs.extra_wom.clone()
 
-    
-
     // material
 
-    this.baseMat = type === 'man' ? mat.base0 : mat.base1;
-    this.pullMat = new THREE.MeshBasicMaterial({ color: type === 'man' ? 0x010044 : 0x1c1c1c, wireframe:false });
-    this.skinMat = new THREE.MeshBasicMaterial({ color: type === 'man' ? 0xb1774f : 0x895837, wireframe:false });
+    //this.mapSkinMat = new THREE.MeshBasicMaterial({ map:type === 'man' ? txt.man : txt.wom, skinning:true, wireframe:debug });
+    this.mapMat = new THREE.MeshBasicMaterial({ map:type === 'man' ? txt.man : txt.wom, wireframe:this.debug });
+    this.mapSkinMat = this.mapMat.clone();
+    this.mapSkinMat.skinning = true;
 
+    this.pullMat = new THREE.MeshBasicMaterial({ color: type === 'man' ? 0x010044 : 0x1c1c1c, wireframe:this.debug });
+    this.skinMat = new THREE.MeshBasicMaterial({ color: type === 'man' ? 0xb1774f : 0x895837, wireframe:this.debug });
+    //this.debugMat = new THREE.MeshBasicMaterial({ color:0xFF0000, wireframe:true });
 
-
-
-    this.mesh.material = type === 'man' ? mat.skin0 : mat.skin1;
-    //a0.material = type === 'man' ? mat.base0 : mat.base1;
-    a1.material = this.baseMat
-    a2.material = this.baseMat;
+    this.mesh.material = this.mapSkinMat;
+    a1.material = this.mapMat;
+    a2.material = this.mapMat;
 
     a1.add( a2 );
     //this.b.abdomen.add( a0 );
     this.b.head.add( a1 );
 
-    if(type === 'man') this.mesh.play(0);
-    else this.mesh.play(1);
+    /*if(type === 'man') this.mesh.play(0);
+    else this.mesh.play(1);*/
+
+    //this.mesh.position.copy( pos );
 
     this.mesh.position.copy( pos );
 
-    var pos = []
+    var pos = [];
 
-    var mat = new THREE.MeshBasicMaterial({ color:0xFF0000, wireframe:true });
+
 
     this.footR = meshs.foot.clone();
     this.footL = meshs.foot.clone();
 
-    this.footR.rotation.set( 180*torad,180*torad, 0*torad );
-    this.footL.rotation.set( 180*torad,180*torad, 0*torad );
+    this.footR.rotation.set( 180*torad, 180*torad, 0 );
+    this.footL.rotation.set( 180*torad, 180*torad, 0 );
 
-    this.footR.material = this.baseMat;
-    this.footL.material = this.baseMat;
+    this.footR.material = this.mapMat;
+    this.footL.material = this.mapMat;
 
     //this.b.rFoot.add( this.footR );
     //this.b.lFoot.add( this.footL );
@@ -97,10 +95,10 @@ V.Model = function ( Scene, type, meshs, mat, pos ) {
     // new Leg
 
     this.legL = new THREE.Tubex( { start:[0,0,0], end:[0,1,0], numSegment:3 }, 12, tSize );
-    this.legmeshL = new THREE.Mesh( this.legL, debug ? mat : this.pullMat );
+    this.legmeshL = new THREE.Mesh( this.legL, this.pullMat );
 
     this.legR = new THREE.Tubex( { start:[0,0,0], end:[0,1,0], numSegment:3 }, 12, tSize );
-    this.legmeshR = new THREE.Mesh( this.legR, debug ? mat : this.pullMat );
+    this.legmeshR = new THREE.Mesh( this.legR, this.pullMat );
 
     Scene.add( this.legmeshL );
     Scene.add( this.legmeshR );
@@ -108,10 +106,10 @@ V.Model = function ( Scene, type, meshs, mat, pos ) {
     // new arm
 
     this.armL = new THREE.Tubex( { start:[0,0,0], end:[0,1,0], numSegment:4 }, 16, tSize );
-    this.armmeshL = new THREE.Mesh( this.armL, debug ? mat : this.pullMat );
+    this.armmeshL = new THREE.Mesh( this.armL, this.pullMat );
 
     this.armR = new THREE.Tubex( { start:[0,0,0], end:[0,1,0], numSegment:4 }, 16, tSize );
-    this.armmeshR = new THREE.Mesh( this.armR, debug ? mat : this.pullMat );
+    this.armmeshR = new THREE.Mesh( this.armR, this.pullMat );
 
     Scene.add( this.armmeshL );
     Scene.add( this.armmeshR );
@@ -128,16 +126,31 @@ V.Model = function ( Scene, type, meshs, mat, pos ) {
     //this.helper = new THREE.SkeletonHelper( this.mesh );
     this.helper = new THREE.SkeletonHelper( this.b.hip );
     this.helper.skeleton = this.mesh.skeleton;
-
-    this.helper.visible = debug;
+    this.helper.visible = this.debug;
 
     Scene.add( this.helper );
+    //this.root.add( this.mesh );
+
     Scene.add( this.mesh );
     
 }
 
+
 V.Model.prototype = {
+    setDebug: function(b){
+
+        this.debug = b;
+
+        this.helper.visible = this.debug;
+        this.mapMat.wireframe = this.debug;
+        this.mapSkinMat.wireframe = this.debug;
+        this.pullMat.wireframe = this.debug;
+        this.skinMat.wireframe = this.debug;
+
+    },
     update: function ( ){
+
+        var m;
 
         this.legL.positions[0].setFromMatrixPosition( this.b.lThigh.matrixWorld );
         this.legL.positions[1].setFromMatrixPosition( this.b.lShin.matrixWorld );
@@ -158,25 +171,28 @@ V.Model.prototype = {
         this.legR.updatePath();
 
         this.armL.positions[0].setFromMatrixPosition( this.b.lCollar.matrixWorld );
-        this.armL.positions[1].setFromMatrixPosition( this.b.lShldr.matrixWorld.clone().multiply(this.dda)  )//.add(this.dcs);
-        this.armL.positions[2].setFromMatrixPosition( this.b.lForeArm.matrixWorld.clone().multiply(this.ddx)  );
-        this.armL.positions[3].setFromMatrixPosition( this.b.lHand.matrixWorld  );
+        this.armL.positions[1].setFromMatrixPosition( this.b.lShldr.matrixWorld.clone().multiply(this.dda) );
+        this.armL.positions[2].setFromMatrixPosition( this.b.lForeArm.matrixWorld.clone().multiply(this.ddx) );
 
-        this.handL.position.setFromMatrixPosition( this.b.lHand.matrixWorld );
-        this.handL.quaternion.setFromRotationMatrix( this.b.lHand.matrixWorld );
+        m = this.b.lHand.matrixWorld;
+
+        this.armL.positions[3].setFromMatrixPosition( m );
+        this.handL.position.setFromMatrixPosition( m );
+        this.handL.quaternion.setFromRotationMatrix( m );
 
         this.armL.updatePath();
 
         this.armR.positions[0].setFromMatrixPosition( this.b.rCollar.matrixWorld );
         this.armR.positions[1].setFromMatrixPosition( this.b.rShldr.matrixWorld.clone().multiply(this.ddd) );
         this.armR.positions[2].setFromMatrixPosition( this.b.rForeArm.matrixWorld.clone().multiply(this.ddx) );
-        this.armR.positions[3].setFromMatrixPosition( this.b.rHand.matrixWorld );
 
-        this.handR.position.setFromMatrixPosition( this.b.rHand.matrixWorld );
-        this.handR.quaternion.setFromRotationMatrix( this.b.rHand.matrixWorld );
+        m = this.b.rHand.matrixWorld;
+
+        this.armR.positions[3].setFromMatrixPosition( m );
+        this.handR.position.setFromMatrixPosition( m );
+        this.handR.quaternion.setFromRotationMatrix( m );
 
         this.armR.updatePath();
-
 
         this.helper.update();
 
