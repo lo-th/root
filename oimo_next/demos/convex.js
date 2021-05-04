1d4b13cba970f44891af3b1873d4e97376b242a9
lo-th <3dflashlo@gmail.com>
2021-05-04 13:13:00 +0200

    up

--- oimo_next/demos/convex.js
+++ oimo_next/demos/convex.js
@@ -11,7 +11,7 @@ function demo() {
 
 }
 
-function onComplete( cubeEnv ){
+function onComplete(){
 
     var list = Pool.meshList('diamond');
 
@@ -19,13 +19,12 @@ function onComplete( cubeEnv ){
 
     for( let m in list ){
 
-        let cube = Main.getCube( list[m].geometry );
-
         mat[m] = new Diamond({
             color: n===0 ? 0xffffff : rand( 0x000000, 0xffffff ),
             envMap: Main.getEnvmap()
         },{
-            normalCube: cube,
+            geometry:list[m].geometry,
+            renderer: Main.getRenderer()
         });
 
         n++
@@ -34,22 +33,28 @@ function onComplete( cubeEnv ){
 
     n = 4;
 
+    let bodys = []
+
     while( n-- ){
         for( let m in list ){
 
             b = phy.add( {
-
                 type:'convex',
                 shape: list[m].geometry,
                 pos:[rand(-4,4), rand(3,20), rand(-4,4)],
                 density:1,
                 size:[30],
                 material: mat[m],
-                
+                //auto:true    
             })
+
+            bodys.push( b );
 
         }
 
     }
+
+    var sparkle = new Sparkle({ objectList:bodys, controler:Main.getControler() });
+    phy.addDirect( sparkle );
 
 }
