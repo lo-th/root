a7bc111f55345492b333cca004fac326bdf49e16
lo-th <3dflashlo@gmail.com>
2021-05-20 21:20:07 +0200

    up

--- oimo_next/demos/terrain.js
+++ oimo_next/demos/terrain.js
@@ -3,57 +3,39 @@ var terrain
 function demo() {
 
 	// config physics setting
-	phy.set( {substep:2, gravity:[0,-9.81,0]});
+	// substep > 1 for increase fidelity
+	phy.set( {substep:2, gravity:[0,-9.81,0]})
+
 
 	// add static ground
-	phy.add({ type:'plane', size:[300,1,300], visible:false });
+	phy.add({ type:'plane', size:[300,1,300], visible:false })
 
-	
-	
-
-    // add terrain
+    // create terrain
 
     terrain = new Landscape({ 
 
-    	size : [ 20, 5, 20 ],
-    	sample : [ 32, 32 ],
-    	frequency : [0.03,0.08,0.5],
+    	size: [ 20, 6, 20 ],
+    	sample: [ 32, 32 ],
+    	frequency: [0.05,0.25,0.75],
     	expo: 2,
-    	uv:10,
-    	debuger:true,
+    	uv: 10,
+    	debuger: true,
     	callback: addLand
 
     })
-
-   //phy.getScene( ).add( terrain );
-
 
 }
 
 function addLand (){
 
-	let g = terrain.getTri().clone().toNonIndexed()
-
-	//let index = g.index.array;
+	let g = terrain.geometry.clone().toNonIndexed()
 	let v = g.attributes.position.array;
-
-
 
 	var shapes = []
 
-	let n, vv
+	let n, vv, lng = v.length/18
 
-	for( let i=0; i<v.length/18; i++ ){
-
-	//for( let i=0; i<index.length/3; i++ ){
-
-		/*n = i*3;
-
-		vv = [
-		v[ index[n] ], v[ index[n]+1 ], v[ index[n]+2 ],
-		v[ index[n+1] ], v[ index[n+1]+1 ], v[ index[n+1]+2 ],
-		v[ index[n+2] ], v[ index[n+2]+1 ], v[ index[n+2]+2 ]
-		]*/
+	for( let i=0; i<lng; i++ ){
 
 		n = i*18;
 
@@ -67,30 +49,30 @@ function addLand (){
 		v[ n+15 ], v[ n+16 ], v[ n+17 ]
 		]
 
-
-
 		shapes.push( { type:'convex', v:vv, nogeo:true })
 
 	}
 
-	console.log( v.length/18 )
+	//console.log( v.length/18 )
 
+	// add static terrain
 
 	phy.add({
         type:'compound',
-        pos:[0,0,0 ],
-        rot:[ 0,0,0],
+        pos:[0,0,0],
+        rot:[0,0,0],
         shapes: shapes,
         mesh:terrain,
     })
 
 
-    // add dynamic box
+    // add dynamic body
 
-	let j = 20;
+	let j = 20, s;
 	while(j--){
-		phy.add({  size:[0.2,0.2,0.2], pos:[math.rand(-4,4),6,math.rand(-4,4)], density:1 });
-		phy.add({ type:'sphere', size:[0.1], pos:[math.rand(-4,4),6,math.rand(-4,4)], density:1 });
+		s = math.rand(0.3,0.8)
+		phy.add({  size:[s], pos:[math.rand(-4,4),6,math.rand(-4,4)], density:1 })
+		phy.add({ type:'sphere', size:[s*0.5], pos:[math.rand(-4,4),6,math.rand(-4,4)], density:1 })
 	}
 	 
 }
